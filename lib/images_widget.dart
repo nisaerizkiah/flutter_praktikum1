537026754a185acf777917fd2cce1a30783bd135
nisaerizkiah <nisaerizkiah@gmail.com>
2025-09-01 16:05:14 +0700

    push

---
+++ lib/images_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ImagesWidget extends StatelessWidget {
+  const ImagesWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      'assets/images/logo-poliwangi.png',
+      width: 150,
+      height: 150,
+  );
+ }
+}
+   
